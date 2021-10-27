c04c4d51c26278b499c2caf2eb906d2a0191f1ec
jumpert <juan.perezc@correo.ucu.edu.uy>
2021-10-26 22:02:42 -0300

    Ejercicio Terminado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,14 +8,14 @@ namespace Program
         static void Main(string[] args)
         {
             
-            Node n1 = new Node(new Persona("Ana", 1));
-            Node n2 = new Node(new Persona("Bean", 2));
-            Node n3 = new Node(new Persona("Carl", 3));
-            Node n4 = new Node(new Persona("Delfy", 4));
-            Node n5 = new Node(new Persona("Eli", 5));
-            Node n6 = new Node(new Persona("Fran", 6));
-            Node n7 = new Node(new Persona("Gus", 7));
-            Node n8 = new Node(new Persona("Hec", 8));
+            Node n1 = new Node(new Persona("Ana", 81));
+            Node n2 = new Node(new Persona("Bean", 50));
+            Node n3 = new Node(new Persona("Carl", 47));
+            Node n4 = new Node(new Persona("Delfy", 24));
+            Node n5 = new Node(new Persona("Eli", 25));
+            Node n6 = new Node(new Persona("Fran", 27));
+            Node n7 = new Node(new Persona("Gus", 13));
+            Node n8 = new Node(new Persona("Hec", 14));
             
 
             n1.AddChildren(n2);
